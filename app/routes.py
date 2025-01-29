c7d31fa725686c20de0023e6b95f08d496a4e20d
DavidPro1997 <david_pro1997@outlook.com>
2025-01-29 17:38:11 -0500

    cd

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 from app.services import Switch
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
 
 # Configuración de CORS específica con encabezados y métodos permitidos
-CORS(app, resources={r"/*": {"origins": ["http://dev.mvevip_cotizador.com","https://website.mvevip.com"]}}, 
-     supports_credentials=True, 
-     allow_headers=["Content-Type", "Authorization"],
-     methods=["POST", "OPTIONS", "GET", "DELETE"])
+# CORS(app, resources={r"/*": {"origins": ["http://dev.mvevip_cotizador.com","https://website.mvevip.com"]}}, 
+#      supports_credentials=True, 
+#      allow_headers=["Content-Type", "Authorization"],
+#      methods=["POST", "OPTIONS", "GET", "DELETE"])
 
 
 @app.route('/')
